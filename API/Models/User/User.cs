2617542074546c032732bbce13dd06a0a5faff65
Kerimcan <464639@student.fontys.nl>
2023-10-29 20:06:33 +0100

    fix: typo

--- API/Models/User/User.cs
+++ API/Models/User/User.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string username { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
--- API/Models/User/UserDTO.cs
+++ API/Models/User/UserDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UserDTO
     {
-        public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
