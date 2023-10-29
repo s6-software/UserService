9a469aa8170a5825ba01b5206a52c8a467f80625
Kerimcan <464639@student.fontys.nl>
2023-10-29 12:06:56 +0100

    feat: sql server connection

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ namespace API
 
             builder.Services.AddControllers();
             builder.Services.AddDbContext<TodoContext>(opt =>
-                opt.UseInMemoryDatabase("TodoList"));
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
