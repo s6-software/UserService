afc00b2f3e1c30c0821630fe3d53ef6c8ddda5fa
Kerimcan <464639@student.fontys.nl>
2023-11-01 00:30:20 +0100

    feat: unit tests init

---
+++ UserTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UserTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);   
+        }
+    }
+}
